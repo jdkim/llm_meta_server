6f3db55bb285eaa21b5f6446dfcaa882cc313b8d
dhq_boiler <dhq_boiler@live.jp>
2025-09-03 17:37:46 +0900

    Remove unimplemented methods temporality

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,25 +6,4 @@ class User < ApplicationRecord
   # has_many :llm_api_keys, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
-
-  def self.from_omniauth(auth)
-  end
-
-  # APIキーを追加
-  def add_llm_apikey(llm_type, plain_api_key)
-  end
-
-  # APIキーを更新
-  def update_llm_apikey(key_id, new_plain_api_key)
-  end
-
-  # APIキーを削除
-  def remove_llm_apikey(key_id)
-  end
-
-  private
-
-  # 関連データのクリーンアップ
-  def cleanup_related_data(llm_api_key)
-  end
 end
