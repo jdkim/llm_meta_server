28937ed5a76688ec75018bc78611ed51f0c1fbaf
dhq_boiler <dhq_boiler@live.jp>
2025-09-03 17:44:11 +0900

    Add user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user with email" do
+    user = User.new(email: "test@example.com", password: "password123")
+    assert user.valid?
+  end
+
+  test "invalid user without email" do
+    user = User.new(password: "password123")
+    assert_not user.valid?
+    assert_includes user.errors[:email], "can't be blank"
+  end
+
+  test "invalid user with duplicate email" do
+    User.create!(email: "test@example.com", password: "password123")
+    user = User.new(email: "test@example.com", password: "password123")
+    assert_not user.valid?
+    assert_includes user.errors[:email], "has already been taken"
+  end
 end
