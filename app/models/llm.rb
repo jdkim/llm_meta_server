2c7e1e4c15972dcd5e6ff606b127d0cb6315f04a
dhq_boiler <dhq_boiler@live.jp>
2025-09-04 13:57:17 +0900

    Add LlmModel

--- app/models/llm.rb
+++ app/models/llm.rb
@@ -1,6 +1,6 @@
 class Llm < ApplicationRecord
   # has_many :llm_api_keys, dependent: :destroy
-  # has_many :llm_models, dependent: :destroy
+  has_many :llm_models, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
