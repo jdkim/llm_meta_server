de868417c6461fe14b7df9abb32cd069ac039da3
dhq_boiler <dhq_boiler@live.jp>
2025-09-05 09:46:22 +0900

    Remove for Password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[google_oauth2]
+  devise :omniauthable, omniauth_providers: %i[google_oauth2]
 
   has_many :llm_api_keys, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      user.google_id = auth.uid
+    end
+  end
 end
