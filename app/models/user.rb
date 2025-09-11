e102642d690f05dd1b2354f0026774cc15b0814a
dhq_boiler <dhq_boiler@live.jp>
2025-09-11 15:36:29 +0900

    Add methods into user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,34 @@ class User < ApplicationRecord
       user.google_id = auth.uid
     end
   end
+
+  # Method to determine authentication provider
+  def authentication_provider
+    return :google_oauth2 if google_id.present?
+    # Add conditions here when adding other IdPs in the future
+    # return :azure_oauth2 if azure_id.present?
+    # return :github if github_id.present?
+
+    :unknown
+  end
+
+  # Check if user is a Google SSO user
+  def google_sso_user?
+    authentication_provider == :google_oauth2
+  end
+
+  # Check if user is an SSO (Single Sign-On) user
+  def sso_user?
+    authentication_provider != :unknown
+  end
+
+  # Get authentication provider display name
+  def authentication_provider_name
+    case authentication_provider
+    when :google_oauth2
+      'Google'
+    else
+      'Unknown'
+    end
+  end
 end
