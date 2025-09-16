a23213f49991e2f2597b66c54a97eba220357c21
dhq_boiler <dhq_boiler@live.jp>
2025-09-16 13:25:37 +0900

    Specified redirect destination

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -5,6 +5,6 @@ class UserControllerTest < ActionDispatch::IntegrationTest
   test "should redirect when not authenticated" do
     get "/profile"
     # Verify that some redirect occurs when not authenticated
-    assert_response :redirect
+    assert_redirected_to root_path
   end
 end
