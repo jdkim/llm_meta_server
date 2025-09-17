cc2b43c3aca32843b88c05a3c56a4d33b2b8f20d
dhq_boiler <dhq_boiler@live.jp>
2025-09-17 14:47:06 +0900

    Redirect signed-in users on the home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       # Logged-in users will be redirected to their profile page.
-      # redirect_to user_profile_path
+      redirect_to user_path(current_user)
     end
   end
 end
