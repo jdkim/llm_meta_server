63695aeb83b37ab9f43a7b5204860e866edd0f13
dhq_boiler <dhq_boiler@live.jp>
2025-09-29 14:05:02 +0900

    Fix comment

--- app/controllers/llm_api_keys_controller.rb
+++ app/controllers/llm_api_keys_controller.rb
@@ -1,7 +1,7 @@
 class LlmApiKeysController < ApplicationController
   before_action :authenticate_user!
 
-  # GET /user/:user_id/keys
+  # GET /user/:user_id/llm_api_keys
   def index
     @llm_api_keys = current_user.llm_api_keys
   end
