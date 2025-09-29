8baef364cae91e09b59e72c8e97c738be21dc7c0
dhq_boiler <dhq_boiler@live.jp>
2025-09-29 13:56:45 +0900

    Stop using the instance variable @user and switch to current_user

--- app/controllers/llm_api_keys_controller.rb
+++ app/controllers/llm_api_keys_controller.rb
@@ -1,16 +1,8 @@
 class LlmApiKeysController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user
 
   # GET /user/:user_id/keys
   def index
-    @llm_api_keys = @user.llm_api_keys
-  end
-
-
-  private
-
-  def set_user
-    @user = current_user
+    @llm_api_keys = current_user.llm_api_keys
   end
 end
