3c254912b6dda9bd6929ab210c6058ec56ca4ab2
dhq_boiler <dhq_boiler@live.jp>
2025-09-29 16:02:39 +0900

    Remove unused code

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,4 @@ class User < ApplicationRecord
       user.google_id = auth.uid
     end
   end
-
-  # Add API key
-  def add_llm_apikey(llm_type, api_key, description)
-    # Encrypt and save new API key
-    encrypted_key = ApiKeyEncrypter.new.encrypt(api_key)
-
-    llm_api_keys.create!(
-      uuid: SecureRandom.uuid,
-      llm_type: llm_type,
-      encrypted_api_key: encrypted_key,
-      description: description
-    )
-  end
 end
