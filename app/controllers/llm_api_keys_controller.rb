cbb255f8ed87539801acde637883099904a7dc7b
dhq_boiler <dhq_boiler@live.jp>
2025-09-29 15:19:15 +0900

    Implemented adding api key

--- app/controllers/llm_api_keys_controller.rb
+++ app/controllers/llm_api_keys_controller.rb
@@ -5,4 +5,22 @@ class LlmApiKeysController < ApplicationController
   def index
     @llm_api_keys = current_user.llm_api_keys
   end
+
+  # POST /user/:user_id/llm_api_keys
+  def create
+    llm_type = params[:llm_type]
+    api_key = params[:api_key]
+    description = params[:description]
+
+    if llm_type.present? && api_key.present?
+      begin
+        current_user.add_llm_apikey(llm_type, api_key, description)
+        redirect_to user_llm_api_keys_path, notice: "API key has been added successfully"
+      rescue => e
+        redirect_to user_llm_api_keys_path, method: :get, alert: "Failed to add API key: #{e.message}"
+      end
+    else
+      redirect_to user_llm_api_keys_path, alert: "Please enter LLM type and API key"
+    end
+  end
 end
