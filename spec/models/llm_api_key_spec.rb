a2a9c090b674b2c266b377bb51b00435dc4cbc05
dhq_boiler <dhq_boiler@live.jp>
2025-09-30 13:39:22 +0900

    Fix tests

--- spec/models/llm_api_key_spec.rb
+++ spec/models/llm_api_key_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe LlmApiKey, type: :model do
+  let(:plain_api_key) { "plain_text_key_example" }
+  let(:api_key_encrypter) { instance_double(ApiKeyEncrypter) }
+  let(:ciphertext_blob) { "encrypted_api_key" }
+  let(:base64_ciphertext) { Base64.encode64(ciphertext_blob) }
   let(:user) { User.create!(email: "test@example.com", google_id: 1) }
   let(:llm_api_key) { LlmApiKey.new(params) }
+
+  before do
+    allow(ApiKeyEncrypter).to receive(:new).and_return(api_key_encrypter)
+    allow(api_key_encrypter).to receive(:encrypt).with(plain_api_key)
+                                          .and_return(base64_ciphertext)
+  end
 
   describe '#valid?' do
     subject { llm_api_key }
@@ -11,7 +21,7 @@ RSpec.describe LlmApiKey, type: :model do
       let(:params) {
         {
           llm_type: "openai",
-          api_key: "plain_text_key_example",
+          api_key: plain_api_key,
           user: user
         }
       }
@@ -21,7 +31,7 @@ RSpec.describe LlmApiKey, type: :model do
                                  user: user,
                                  uuid: kind_of(String),
                                  llm_type: "openai",
-                                 encrypted_api_key: kind_of(String)
+                                 encrypted_api_key: base64_ciphertext
                                )
       }
     end
