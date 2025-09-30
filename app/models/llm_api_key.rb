f17a84a89a0d41fb7cdf6d642090034dbc2de089
dhq_boiler <dhq_boiler@live.jp>
2025-09-30 11:37:50 +0900

    Remove presence: true validation

--- app/models/llm_api_key.rb
+++ app/models/llm_api_key.rb
@@ -1,7 +1,7 @@
 class LlmApiKey < ApplicationRecord
   belongs_to :user
 
-  validates :uuid, presence: true, uniqueness: true
+  validates :uuid, uniqueness: true
   validates :llm_type, presence: true
   validates :description, length: { maximum: 255 }, allow_blank: true
 
