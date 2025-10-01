2d013a58e538254b934b19df9b525bcbe3ed4c68
dhq_boiler <dhq_boiler@live.jp>
2025-10-01 15:13:23 +0900

    Add test

--- spec/models/llm_api_key_spec.rb
+++ spec/models/llm_api_key_spec.rb
@@ -43,4 +43,37 @@ RSpec.describe LlmApiKey, type: :model do
       end
     end
   end
+
+  describe '#update' do
+    subject { llm_api_key }
+
+    context 'when updating with api_key, encrypted_api_key is updated and api_key is not retained' do
+      let(:params) {
+        {
+          llm_type: "openai",
+          api_key: plain_api_key,
+          user: user
+        }
+      }
+
+      let(:new_plain_api_key) { "new_plain_text_key_example" }
+      let(:new_base64_ciphertext) { "new_dummy_base64_encoded_encrypted_api_key" }
+
+      before do
+        allow(api_key_encrypter).to receive(:encrypt).with(new_plain_api_key)
+                                              .and_return(new_base64_ciphertext)
+        llm_api_key.update!(api_key: new_plain_api_key)
+      end
+
+      it {
+        is_expected.to have_attributes(
+                         user: user,
+                         uuid: kind_of(String),
+                         llm_type: "openai",
+                         encrypted_api_key: new_base64_ciphertext
+                       )
+        expect(llm_api_key.api_key).to be_nil
+      }
+    end
+  end
 end
