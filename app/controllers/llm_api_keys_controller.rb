86aa179971b8318f89d27ebbbca76c6cb849ff1b
dhq_boiler <dhq_boiler@live.jp>
2025-10-02 15:03:29 +0900

    Use memoization

--- app/controllers/llm_api_keys_controller.rb
+++ app/controllers/llm_api_keys_controller.rb
@@ -18,14 +18,12 @@ class LlmApiKeysController < ApplicationController
 
   # PATCH/PUT /user/:user_id/llm_api_keys/:id
   def update
-    # Temporarily store in an instance variable to retain dirty information
-    @llm_api_key = llm_api_key
-    @llm_api_key.update!(llm_api_key_params)
-    if @llm_api_key.saved_change_to_encrypted_api_key? && @llm_api_key.saved_change_to_description?
+    llm_api_key.update!(llm_api_key_params)
+    if llm_api_key.saved_change_to_encrypted_api_key? && llm_api_key.saved_change_to_description?
       redirect_to user_llm_api_keys_path, notice: "API key and description have been updated successfully"
-    elsif @llm_api_key.saved_change_to_encrypted_api_key?
+    elsif llm_api_key.saved_change_to_encrypted_api_key?
       redirect_to user_llm_api_keys_path, notice: "API key has been updated successfully"
-    elsif @llm_api_key.saved_change_to_description?
+    elsif llm_api_key.saved_change_to_description?
       redirect_to user_llm_api_keys_path, notice: "Description of API key has been updated successfully"
     else
       redirect_to user_llm_api_keys_path, alert: "Please enter a new API key or description"
@@ -43,7 +41,8 @@ class LlmApiKeysController < ApplicationController
   end
 
   def llm_api_key
-    current_user.llm_api_keys.find(params[:id])
+    # Temporarily store in an instance variable to retain dirty information
+    @llm_api_key ||= current_user.llm_api_keys.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to user_path(current_user), alert: "The specified API key was not found"
   end
