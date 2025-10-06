3fc74be8334ff7cea8e59e36d175397c73981a95
dhq_boiler <dhq_boiler@live.jp>
2025-10-06 13:33:04 +0900

    Implemented EncryptableApiKey class

---
+++ app/services/encryptable_api_key.rb
@@ -0,0 +1,24 @@
+class EncryptableApiKey
+  # Specify either plain_api_key or encrypted_api_key (both cannot be specified)
+  def initialize(plain_api_key: nil, encrypted_api_key: nil)
+    raise ArgumentError, "Specify either plain_api_key or encrypted_api_key" if plain_api_key && encrypted_api_key
+    raise ArgumentError, "Either plain_api_key or encrypted_api_key must be specified" if !plain_api_key && !encrypted_api_key
+    @plain_src     = plain_api_key if plain_api_key.present?
+    @encrypted_src = encrypted_api_key if encrypted_api_key.present?
+  end
+
+  # Plain text (if not available, decrypt and memoize)
+  def plain_api_key
+    @plain ||= @plain_src ||  ApiKeyDecrypter.new.decrypt(@encrypted_src)
+  end
+
+  # Encrypted (if not available, encrypt and memoize)
+  def encrypted_api_key
+    @encrypted ||= @encrypted_src ||  ApiKeyDecrypter.new.encrypt(@plain_src)
+  end
+
+  # For debugging: considering confidentiality
+  def inspect
+    '#<EncryptableApiKey plain_api_key="[REDACTED]" encrypted_api_key="[REDACTED]">'
+  end
+end
