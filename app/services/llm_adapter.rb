0c13fc73f9d1d1081649cd2d2a034596655dc4ce
dhq_boiler <dhq_boiler@live.jp>
2025-10-10 09:59:19 +0900

    Refactor

--- app/services/llm_adapter.rb
+++ app/services/llm_adapter.rb
@@ -1,14 +1,9 @@
 require "llm"
 class LlmAdapter
-  def initialize(llm_type, encryptable_api_key)
-    @llm_strategy = select_strategy llm_type
-    @encryptable_api_key = encryptable_api_key
-  end
-
-  def call(model_name, prompt)
-    llm = @llm_strategy.llm
-
+  def self.call(llm_type, encryptable_api_key, model_name, prompt)
+    llm = select_llm llm_type, encryptable_api_key
     model_id = model_id(llm, model_name)
+
     bot  = LLM::Bot.new(llm, model: model_id)
     messages = bot.chat do |_prompt|
       _prompt.user prompt
@@ -19,23 +14,23 @@ class LlmAdapter
 
   private
 
-  def model_id(llm, model_name)
+  def self.model_id(llm, model_name)
     model = llm.models.all.find { |m| m.id == model_name }
     raise ModelNotFoundError.new(model_name) unless model
 
     model.id
   end
 
-  def select_strategy(llm_type)
+  def self.select_llm(llm_type, encryptable_api_key)
     case llm_type
     when "ollama"
-      LlmStrategies::OllamaStrategy.llm @encryptable_api_key
+      LlmStrategies::OllamaStrategy.llm encryptable_api_key
     when "openai"
-      LlmStrategies::OpenaiStrategy.llm @encryptable_api_key
+      LlmStrategies::OpenaiStrategy.llm encryptable_api_key
     when "anthropic"
-      LlmStrategies::AnthropicStrategy.llm @encryptable_api_key
+      LlmStrategies::AnthropicStrategy.llm encryptable_api_key
     when "google"
-      LlmStrategies::GoogleStrategy.llm @encryptable_api_key
+      LlmStrategies::GoogleStrategy.llm encryptable_api_key
     else
       raise NotSupportedLlmError.new(llm_type)
     end
