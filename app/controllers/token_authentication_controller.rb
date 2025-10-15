bcd030a21358fd6cbb99bf31b5e8a1be77a0bcfd
dhq_boiler <dhq_boiler@live.jp>
2025-10-15 17:04:54 +0900

    Extract methods

--- app/controllers/token_authentication_controller.rb
+++ app/controllers/token_authentication_controller.rb
@@ -2,30 +2,13 @@ class TokenAuthenticationController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [ :create ]
   skip_before_action :authenticate_user!, only: [ :create ]
 
-  # POST /auth/token
+  JWT_ALGORITHM = "HS256"
+
   def create
     jwt_payload = decode_jwt(params[:token])
     user = User.find_by!(google_id: jwt_payload["google_id"])
 
-    case jwt_payload["action"]
-    when "llm_api_keys"
-      @llm_api_keys = user.llm_api_keys
-      render json: {
-        llm_api_keys: @llm_api_keys.map { |api_key|
-          {
-            uuid: api_key.uuid,
-            llm_type: api_key.llm_type,
-            description: api_key.description,
-            created_at: api_key.created_at,
-            updated_at: api_key.updated_at
-          }
-        }
-      }
-      # when 'relay'
-      #   redirect_to user_relay_path(user)
-    else
-      render json: { error: "Invalid redirect destination" }, status: :bad_request
-    end
+    handle_action jwt_payload["action"], user
   rescue JWT::DecodeError => e
     render json: { error: "Invalid token", message: e.message }, status: :unauthorized
   rescue ActiveRecord::RecordNotFound
@@ -35,6 +18,28 @@ class TokenAuthenticationController < ApplicationController
   private
 
   def decode_jwt(token)
-    JWT.decode(token, Rails.application.credentials.secret_key_base, true, algorithm: "HS256").first
+    JWT.decode(
+      token,
+      Rails.application.credentials.secret_key_base,
+      true,
+      algorithm: JWT_ALGORITHM
+    ).first
+  end
+
+  def handle_action(action, user)
+    case action
+    when "llm_api_keys"
+      render_llm_api_keys user
+    else
+      render json: { error: "Invalid redirect destination" }, status: :bad_request
+    end
+  end
+
+  def render_llm_api_keys(user)
+    render json: {
+      llm_api_keys: user.llm_api_keys.as_json(
+        only: [:uuid, :llm_type, :description, :created_at, :updated_at]
+      )
+    }
   end
 end
