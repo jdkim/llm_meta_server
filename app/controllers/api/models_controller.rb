94fab65c2d59ecfa7ed19d3ee66f459f60c259c9
dhq_boiler <dhq_boiler@live.jp>
2025-10-17 14:22:59 +0900

    Bugfix

--- app/controllers/api/models_controller.rb
+++ app/controllers/api/models_controller.rb
@@ -6,7 +6,7 @@ class Api::ModelsController < ApiController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    uuid = expected_params[0]
+    uuid = expected_params
 
     llm_api_key = current_user.llm_api_keys.find_by!(uuid: uuid)
     models = LlmRbFacade.models llm_api_key
