a561f9565bd0d06fc88accd5dd76957f9bacdb51
dhq_boiler <dhq_boiler@live.jp>
2025-10-27 13:52:11 +0900

    Run rubocop -A

--- app/models/llm_model_map.rb
+++ app/models/llm_model_map.rb
@@ -6,7 +6,7 @@ class LlmModelMap
     "gpt-4o-mini" => "gpt-4o-mini",
     "gpt-4-turbo" => "gpt-4-turbo",
     "gpt-3-5-turbo" => "gpt-3.5-turbo",
-    "gpt-3-5-turbo-16k" => "gpt-3.5-turbo-16k",
+    "gpt-3-5-turbo-16k" => "gpt-3.5-turbo-16k"
   }
   MODEL_MAP_ANTHROPIC = {
     # Anthropic Models
@@ -19,7 +19,7 @@ class LlmModelMap
     "claude-3-5-haiku-20241022" => "claude-3-5-haiku-20241022",
     "claude-3-opus-20240229" => "claude-3-opus-20240229",
     "claude-3-sonnet-20240229" => "claude-3-sonnet-20240229",
-    "claude-3-haiku-20240307" => "claude-3-haiku-20240307",
+    "claude-3-haiku-20240307" => "claude-3-haiku-20240307"
   }
   MODEL_MAP_GOOGLE = {
     # Google Gemini Models
