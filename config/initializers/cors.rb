d96edfa9c9a2de2dda21576ca6512f46c8032512
dhq_boiler <dhq_boiler@live.jp>
2025-11-10 16:37:51 +0900

    Further narrow down the origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,9 +4,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Development environment
-    origins "localhost:3001",
-            "127.0.0.1:3001",
-            "http://localhost:3001",
+    origins "http://localhost:3001",
             "http://127.0.0.1:3001"
 
     resource "/api/*",
