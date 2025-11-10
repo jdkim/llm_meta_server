341d4f58f28802c8894a2df81f8f147227864b79
dhq_boiler <dhq_boiler@live.jp>
2025-11-10 16:08:23 +0900

    Remove unnecessary origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,10 +4,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Development environment
-    origins "localhost:3000", "localhost:8080", "localhost:3001",
-            "127.0.0.1:3000", "127.0.0.1:8080", "127.0.0.1:3001",
-            "http://localhost:3000", "http://localhost:8080", "http://localhost:3001",
-            "http://127.0.0.1:3000", "http://127.0.0.1:8080", "http://127.0.0.1:3001"
+    origins "localhost:3001",
+            "127.0.0.1:3001",
+            "http://localhost:3001",
+            "http://127.0.0.1:3001"
 
     resource "/api/*",
       headers: :any,
